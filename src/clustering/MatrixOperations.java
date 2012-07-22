fac91f99714419ae7613b442a5154ef406e3b3ac
Gaurav Singh <gauravonline20@gmail.com>
2012-07-22 14:41:27 +0530

    small fix

--- src/clustering/MatrixOperations.java
+++ src/clustering/MatrixOperations.java
@@ -7,7 +7,7 @@ public class MatrixOperations {
 		int [][]matrix3=new int[matrix1.length][matrix1[0].length];
 		for(int i=0;i<matrix1.length;i++){
 			for(int j=0;j<matrix1[i].length;j++){
-				matrix3[i][j]=(value_of_n*matrix2[i][j]+matrix1[i][j])/2;
+				matrix3[i][j]=(value_of_n*matrix2[i][j]+matrix1[i][j])/(value_of_n+1);
 				
 			}
 		}
